fd7ce45d3ce38a91b8d199e626ca6453f2758536
KJwacky <112001929+KJwacky@users.noreply.github.com>
2026-02-01 12:31:12 -0700

    vent ghost

--- godot-project/scripts/sewer_vent.gd
+++ godot-project/scripts/sewer_vent.gd
@@ -2,20 +2,23 @@ extends Node2D
 
 @onready var player = get_parent().get_node("Player")
 @export var speed = 1
-# Called when the node enters the scene tree for the first time.
+
+@onready var belchScene = preload("res://scenes/belch.tscn")
+
+
 func _ready() -> void:
 	$ActivateAnim.play("activate")
-	await get_tree().create_timer(10).timeout
-	queue_free()
 
 func _process(delta: float) -> void:
 	position.x -= 250 * delta * speed
 	position.x = round(position.x)
+	if position.x < -1000:
+		queue_free()
 
 
 func _on_kill_timer_timeout() -> void:
-	if abs(player.position.x - position.x) < 20:
-		player.Die("Sewer")
+	var belchInstance = belchScene.instantiate()
+	add_child(belchInstance)
 
 func stop_movement()-> void:
 	speed = 0
